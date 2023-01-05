d2b813abe8718960a6e2b3744b01de74fe7e0a62
hharsh090 <122008598+hharsh090@users.noreply.github.com>
2023-01-05 15:05:36 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('../mainPage.ejs')
+})
+
+
+
+app.listen(port, () => {
+    console.log('8080')
+})
