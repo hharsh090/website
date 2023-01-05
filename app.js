3fdaf6090801045ba2c91a1a3b9ffc09b9471ae6
hharsh090 <122008598+hharsh090@users.noreply.github.com>
2023-01-05 16:03:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
+const path = require("path");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
-
-
-app.get('/', (req, res) => {
-    res.render('../mainPage.ejs')
-})
-
-
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "./" });
+});
 
 app.listen(port, () => {
-    console.log('8080')
-})
-
-// deploy
+  console.log("8080");
+});
